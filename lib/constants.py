f76d0f889bcadd483ff30a168a46d8c3433abd16
Sean Siebert <seannieseansean@gmail.com>
2016-12-25 11:16:52 -0500

    Updated version number.

--- lib/constants.py
+++ lib/constants.py
@@ -6,7 +6,7 @@ DATE_FORMAT = "%Y-%m-%d"
 LOGGER_FORMAT = "%(name)-11s %(levelname)-8s %(message)s"
 MAX_RESULTS = 100
 
-VERSION = '3.0.0 -- Forked from 2.4.6'
+VERSION = '3.0.1 -- Forked from 2.4.6'
 
 CONFIG_FILE = '''[Settings]
 file_name = id
